b5d2c4b8f8e4fe84e169640256440352c4f50608
HankyZhang <296127042@qq.com>
2025-11-05 04:10:04 +0000

    Update SM2 DH to use strongSwan 6.x key_exchange_t API and GmSSL 3.x sm2_ecdh API

--- src/libstrongswan/plugins/gmsm/gmsm_sm2_dh.c
+++ src/libstrongswan/plugins/gmsm/gmsm_sm2_dh.c
@@ -48,32 +48,28 @@ struct private_gmsm_sm2_dh_t {
 	chunk_t shared_secret;
 };
 
-METHOD(key_exchange_t, set_other_public_value, bool,
+METHOD(key_exchange_t, set_public_key, bool,
 	private_gmsm_sm2_dh_t *this, chunk_t value)
 {
-	SM2_POINT peer_public;
+	SM2_POINT peer_point;
 	uint8_t shared_key[32];
 
 	if (!value.len || value.len != 65)
 	{
-		DBG1(DBG_LIB, "invalid SM2 public value length: %zu", value.len);
+		DBG1(DBG_LIB, "invalid SM2 public value length: %zu (expected 65)", value.len);
 		return FALSE;
 	}
 
-	/* Convert peer's public key from octets to SM2_POINT */
-	if (sm2_point_from_uncompressed_octets(&peer_public, value.ptr) != 1)
+	/* GmSSL 3.x sm2_ecdh signature: 
+	 * int sm2_ecdh(const SM2_KEY *key, const uint8_t *peer_public, size_t peer_public_len, SM2_POINT *out) */
+	if (sm2_ecdh(&this->private_key, value.ptr, value.len, &peer_point) != 1)
 	{
-		DBG1(DBG_LIB, "failed to parse SM2 peer public key");
+		DBG1(DBG_LIB, "SM2 ECDH computation failed");
 		return FALSE;
 	}
 
-	/* Perform SM2 key exchange to derive shared secret */
-	/* Note: GmSSL sm2_ecdh uses the private key and peer's public key */
-	if (sm2_ecdh(&this->private_key, &peer_public, shared_key) != 1)
-	{
-		DBG1(DBG_LIB, "SM2 ECDH failed");
-		return FALSE;
-	}
+	/* Extract X coordinate as shared secret (32 bytes) */
+	memcpy(shared_key, &peer_point.x, 32);
 
 	chunk_free(&this->shared_secret);
 	this->shared_secret = chunk_clone(chunk_create(shared_key, 32));
@@ -82,32 +78,7 @@ METHOD(key_exchange_t, set_other_public_value, bool,
 	return TRUE;
 }
 
-METHOD(key_exchange_t, get_other_public_value, bool,
-	private_gmsm_sm2_dh_t *this, chunk_t *value)
-{
-	/* Not typically used in strongSwan, but return our public value */
-	*value = chunk_clone(chunk_create(this->public_value, 65));
-	return TRUE;
-}
-
-METHOD(key_exchange_t, set_private_value, bool,
-	private_gmsm_sm2_dh_t *this, chunk_t value)
-{
-	/* Generate new SM2 key pair */
-	if (sm2_key_generate(&this->private_key) != 1)
-	{
-		DBG1(DBG_LIB, "SM2 key generation failed");
-		return FALSE;
-	}
-
-	/* Extract public key as uncompressed octets */
-	sm2_point_to_uncompressed_octets(&this->private_key.public_key, this->public_value);
-
-	DBG3(DBG_LIB, "SM2 DH private/public key pair generated");
-	return TRUE;
-}
-
-METHOD(key_exchange_t, get_my_public_value, bool,
+METHOD(key_exchange_t, get_public_key, bool,
 	private_gmsm_sm2_dh_t *this, chunk_t *value)
 {
 	*value = chunk_clone(chunk_create(this->public_value, 65));
@@ -127,7 +98,7 @@ METHOD(key_exchange_t, get_shared_secret, bool,
 	return TRUE;
 }
 
-METHOD(key_exchange_t, get_dh_group, key_exchange_method_t,
+METHOD(key_exchange_t, get_method, key_exchange_method_t,
 	private_gmsm_sm2_dh_t *this)
 {
 	return this->group;
@@ -161,22 +132,25 @@ gmsm_sm2_dh_t *gmsm_sm2_dh_create(key_exchange_method_t group, ...)
 		.public = {
 			.ke = {
 				.get_shared_secret = _get_shared_secret,
-				.set_other_public_value = _set_other_public_value,
-				.get_my_public_value = _get_my_public_value,
-				.set_private_value = _set_private_value,
-				.get_dh_group = _get_dh_group,
+				.set_public_key = _set_public_key,
+				.get_public_key = _get_public_key,
+				.get_method = _get_method,
 				.destroy = _destroy,
 			},
 		},
 		.group = group,
 	);
 
-	/* Generate initial key pair */
-	if (!set_private_value(this, chunk_empty))
+	/* Generate initial SM2 key pair */
+	if (sm2_key_generate(&this->private_key) != 1)
 	{
-		destroy(this);
+		DBG1(DBG_LIB, "SM2 key generation failed");
+		free(this);
 		return NULL;
 	}
+
+	/* Extract public key as uncompressed octets */
+	sm2_point_to_uncompressed_octets(&this->private_key.public_key, this->public_value);
 
 	return &this->public;
 }
